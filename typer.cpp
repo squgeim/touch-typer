bd93ce673b123b03d1b2ccabe5efad9a84b8c5a1
Shreya Dahal <shreyadahal@gmail.com>
2014-02-15 21:59:16 +0545

    got Glade working

--- typer.cpp
+++ typer.cpp
@@ -1,19 +1,27 @@
 #include<gtkmm.h>
-using namespace std;
+#include<iostream>
 
 int main(int argc, char* argv[]) {
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "com.squgeim.typer");
-
-	Gtk::Window window;
-	window.set_default_size(600,300);
-	window.set_title("Touch Typer");
-
-	Gtk::Box box;
-	Gtk::Button btn("Click here");
-	box.pack_start(btn);
-	window.add(box);
-	btn.show();
-	box.show();
-
-	return app->run(window);
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create();
+	Gtk::Window *mainwin=0;
+	
+	try {
+		builder->add_from_file("glade.xml");
+	}
+	catch(const Glib::FileError& ex) {
+		std::cerr<<"FileError: "<<ex.what()<<std::endl;
+	}
+	catch(const Glib::MarkupError& ex) {
+		std::cerr<<"MarkupError: "<<ex.what()<<std::endl;
+	}
+	catch(const Gtk::BuilderError& ex) {
+		std::cerr<<"BuilderError: "<<ex.what()<<std::endl;
+	}
+	
+	builder->get_widget("main",mainwin);
+	if(mainwin) {
+		return app->run(*mainwin);
+	}
+	else return -1;
 }
